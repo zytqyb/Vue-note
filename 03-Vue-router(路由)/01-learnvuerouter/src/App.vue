7eec10a43c9244aba20ac31e832a19d1223f1899
zytqyb <1713684374@qq.com>
2021-03-24 19:29:47 +0800

    添加tabbar项目代码

--- 03-Vue-router(路由)/01-learnvuerouter/src/App.vue
+++ 03-Vue-router(路由)/01-learnvuerouter/src/App.vue
@@ -16,7 +16,7 @@
     <button @click="userClick">用户</button>
     <button @click="profileClick">档案</button>
 
-    <keep-alive>
+    <keep-alive exclude="Profile,User">
       <router-view></router-view>
     </keep-alive>
   </div>
---
+++ 03-Vue-router(路由)/learnvuerouter/dist/static/js/2.4a4d6f478b031f22168f.js
@@ -0,0 +1,73 @@
+webpackJsonp([2],{
+
+/***/ "jyJz":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/selector.js?type=script&index=0!./src/components/User.vue
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ var User = ({
+  name: 'User',
+  computed: {
+    userId: function userId() {
+      return this.$route.params.abc;
+    }
+  }
+});
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-05bf3ec1","hasScoped":true,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/components/User.vue
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('h2',[_vm._v("我是用户")]),_vm._v(" "),_c('p',[_vm._v("我是用户内容")]),_vm._v(" "),_c('h2',[_vm._v(_vm._s(_vm.userId))]),_vm._v(" "),_c('h2',[_vm._v(_vm._s(_vm.$route.params.abc))])])}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ var components_User = (esExports);
+// CONCATENATED MODULE: ./src/components/User.vue
+function injectStyle (ssrContext) {
+  __webpack_require__("olTr")
+}
+var normalizeComponent = __webpack_require__("VU/8")
+/* script */
+
+
+/* template */
+
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = injectStyle
+/* scopeId */
+var __vue_scopeId__ = "data-v-05bf3ec1"
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  User,
+  components_User,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ var src_components_User = __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+
+/***/ "olTr":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+});
+//# sourceMappingURL=2.4a4d6f478b031f22168f.js.map
